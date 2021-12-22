ed997b1bb771b139759ca4fd7ab4c1b3af055aa2
wpidea <85543435+wpidea@users.noreply.github.com>
2021-12-22 00:15:29 -0800

    Delete extract_target.py

--- extract_target.py
+++
@@ -1,8 +0,0 @@
-
-def extract_target(record):
-    target = record.split('"')[1]
-    target = target.split('/')[1]
-    target = target.split('?')[0]
-    target = target.split(' HTTP')[0]
-    return target
-
